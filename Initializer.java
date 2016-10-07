1d71cd7d721fcbce9a2010214695f67980c8ad0d
Thomas Kwak <teekwak@gmail.com>
2016-10-07 10:09:34 -0700

    Initial commit to repository

---
+++ Initializer.java
@@ -0,0 +1,5 @@
+public class Initializer {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+javac Initializer.java
+java Initializer
+
+rm *.class 2> /dev/null
