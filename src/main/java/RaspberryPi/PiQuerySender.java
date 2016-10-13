bf081a095ac817e7170ec7cde2f2361abb10c1a3
Thomas Kwak <teekwak@gmail.com>
2016-10-13 11:29:09 -0700

    PiQuerySender outputs a pretty progress bar

--- src/main/java/RaspberryPi/PiQuerySender.java
+++ src/main/java/RaspberryPi/PiQuerySender.java
@@ -8,8 +8,11 @@ import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import com.google.gson.*;
+
 import java.net.*;
 import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class PiQuerySender {
 	public static String[] keywordsArray = new String[] {
@@ -37,7 +40,7 @@ public class PiQuerySender {
 	};
 
 	// writes results to file in bitVector_keyword_overlapTotal format
-	public static void storeResults(String bitVector, String keyword, int overlapTotal) {
+	public static void storeResults(String bitVector, String keyword, int overlapTotal, int piNumber) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(bitVector);
 		sb.append("_");
@@ -46,7 +49,7 @@ public class PiQuerySender {
 		sb.append(overlapTotal);
 
 		try (
-			FileWriter fw = new FileWriter("pi1_OutputFile.txt", true);
+			FileWriter fw = new FileWriter("pi" + piNumber + "_OutputFile.txt", true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw)) 
 		{
@@ -68,7 +71,7 @@ public class PiQuerySender {
 			querySb.append(")+AND+snippet_number_of_functions:[1+TO+*]+AND+parent:true+AND+snippet_is_innerClass:false+AND+snippet_is_anonymous:false&start=0&fl=id&indent=on&wt=json&rows=1000&started=false&test=false&conciseCount=100&bitvector=");
 			querySb.append(bitvector);
 
-			System.out.println(querySb.toString());
+			// System.out.println(querySb.toString());
 
 			// REALLY GOTTA CHANGE THE URL TO THE CORRECT ONE
 			URL url = new URL(querySb.toString());
@@ -85,7 +88,7 @@ public class PiQuerySender {
 			if(element.isJsonObject()) {
 				// print out single response by key
 				JsonObject response = element.getAsJsonObject();
-				System.out.println("OverlapNumber: " + response.get("OverlapNumber").getAsString());
+				// System.out.println("OverlapNumber: " + response.get("OverlapNumber").getAsString());
 
 
 				// iterate over map (not required)
@@ -97,19 +100,49 @@ public class PiQuerySender {
 
 				return response.get("OverlapNumber").getAsInt();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 		return -1;
 	}
 
+	public static void generateProgressBar(int count, int maxCount) {
+		int numberOfBars = maxCount / 10;
+		int numberOfFilledBars = count / numberOfBars;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("\rCompletion: [");
+		for(int i = 0; i < numberOfFilledBars; i++) {
+			sb.append("=");
+		}
+		sb.append("          ".substring(numberOfFilledBars));
+		sb.append("] ");
+		sb.append(count);
+		sb.append("/");
+		sb.append(maxCount);
+
+		System.out.print(sb.toString());
+	}
+
 	public static void main(String[] args) {
-		File bitVectorFile = new File("java_output/pi1_binaryNumbersList.txt");
 
-		long a = System.currentTimeMillis();
+		System.out.print("Enter the pi # you are working on: ");
+		Scanner sc = new Scanner(System.in);
+		try {
+			int piNumber = sc.nextInt();
 
-		// read bitVectorFile
+			// if binaryNumbersList does not exist for input, throw error
+			if(!new File("java_output/pi" + piNumber + "_binaryNumbersList.txt").exists()) {
+				throw new InputMismatchException("[ERROR]: You entered a number which has no matching file!");
+			}
+
+			File bitVectorFile = new File("java_output/pi" + piNumber + "_binaryNumbersList.txt");
+
+			long a = System.currentTimeMillis();
+
+			// read bitVectorFile
 //		try {
 //			BufferedReader br = new BufferedReader(new FileReader(bitVectorFile));
 //
@@ -122,25 +155,31 @@ public class PiQuerySender {
 //			e.printStackTrace();
 //		}
 
-		// test
-		int counter = 0;
+			// test
+			int counter = 0;
 
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(bitVectorFile));
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(bitVectorFile));
 
-			for(String line; (line = br.readLine()) != null; ) {
-				if(counter < 100) {
-					storeResults(line, keywordsArray[0], sendQuery(keywordsArray[0], line));
-					counter++;
+				for(String line; (line = br.readLine()) != null; ) {
+					if(counter < 100) {
+						storeResults(line, keywordsArray[1], sendQuery(keywordsArray[1], line), piNumber);
+						generateProgressBar(counter, 100);
+						counter++;
+					}
+					generateProgressBar(counter, 100);
 				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 
-		} catch (IOException e) {
-			e.printStackTrace();
+			long b = System.currentTimeMillis();
+
+			System.out.println("\nProcess time: " + (b-a) + "ms");
+
+		} catch (InputMismatchException e) {
+			System.out.println("[ERROR]: You entered something that was not a number...");
 		}
-
-		long b = System.currentTimeMillis();
-
-		System.out.println("Process time: " + (b-a) + "ms");
 	}	
 }
