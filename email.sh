c70dc702d71e2e19bd7a590a042201cfac655f69
Thomas Kwak <teekwak@gmail.com>
2016-10-24 13:23:01 -0700

    Send email when all jars are finished

---
+++ email.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# email when completed
+
+echo
+echo "===== Running email.sh ====="
+echo
+
+keepTrying=true
+
+while [ keepTrying ];
+do
+	count=0
+
+	# find number of jar instances (1 means not running)
+	IFS=$'\n'
+	for line in $(ps ax | grep similarityfunctions)
+	do
+		(( count++ ))
+	done
+
+	# if there is only one command (which would be the ps command) send me an email
+	if [[ $count -lt 2 ]]; 
+		then
+			echo "Hello! We are confirming that the querying process is complete!" | mail -s "Querying is complete!" teekwak@gmail.com
+			keepTrying=false
+	fi
+
+	sleep 300s;
+done
+
+echo 
+echo "===== Process completed. Exiting gracefully... ====="
+echo
