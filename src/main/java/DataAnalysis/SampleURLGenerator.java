4d14610dbb6dec0ce0f3ba25ee2f463c0c0de67b
Thomas Kwak <teekwak@gmail.com>
2016-12-20 20:43:42 -0800

    Bug fix for getting exactly the sample size needed instead of less

--- src/main/java/DataAnalysis/SampleURLGenerator.java
+++ src/main/java/DataAnalysis/SampleURLGenerator.java
@@ -7,6 +7,8 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.*;
@@ -15,19 +17,15 @@ import java.util.*;
  * Created by Kwak
  */
 
-class BoundedBin {
-	double lowerBound;
-	double upperBound;
-	private Map<String, Integer> mapping;
+class Bin {
+	Map<String, Integer> mapping;
 
-	BoundedBin(double l, double u) {
-		this.lowerBound = l;
-		this.upperBound = u;
+	Bin() {
 		this.mapping = new LinkedHashMap<>();
 	}
 
-	void add(String key, int value) {
-		mapping.put(key, value);
+	Map<String, Integer> getMapping() {
+		return this.mapping;
 	}
 
 	int size() {
@@ -40,39 +38,34 @@ class BoundedBin {
 		return size;
 	}
 
-	Map<String, Integer> getMapping() {
-		return this.mapping;
+	void add(String key, int value) {
+		mapping.put(key, value);
 	}
 }
 
-class TrueFalseBin {
-	private String key;
-	private int size;
+class BoundedBin extends Bin {
+	double lowerBound;
+	double upperBound;
 
-	TrueFalseBin(String k, int s) {
-		this.key = k;
-		this.size = s;
-	}
-
-	String getKey() {
-		return this.key;
-	}
-
-	int size() {
-		return this.size;
+	BoundedBin(double l, double u) {
+		this.lowerBound = l;
+		this.upperBound = u;
+		this.mapping = new LinkedHashMap<>();
 	}
 }
 
 public class SampleURLGenerator {
 	private static final int SAMPLE_SIZE = 8600;
-	private static final int POPULATION_SIZE = 10829321;
 	private static Random randomObject;
+	private static Set<String> savedURLs;
 
 	private static int getNumFound(String server, String key, String value) {
 		// query the server once to get the total number
 		int numFound = 0;
 		try {
-			URL url = new URL("http://" + server + ":9551/solr/MoreLikeThisIndex/select?q=parent%3Atrue+AND+" + key + "%3A\"" + URLEncoder.encode(value, "UTF-8") + "\"&rows=0&wt=json&indent=true");
+			URI uri = new URI("http://" + server + ":9551/solr/MoreLikeThisIndex/select?q=parent%3Atrue+AND+" + key + "%3A" + URLEncoder.encode(value, "UTF-8") + "&rows=0&wt=json&indent=true");
+			URL url = uri.toURL();
+
 			StringBuilder responseSb = new StringBuilder();
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 			for (String line; (line = br.readLine()) != null;) {
@@ -88,13 +81,13 @@ public class SampleURLGenerator {
 				JsonObject response = totalResponse.getAsJsonObject("response");
 				numFound = response.get("numFound").getAsInt();
 			}
-		} catch (IOException e) {
+		} catch (IOException|URISyntaxException e) {
 			e.printStackTrace();
 		}
 
 		if(numFound == 0) {
 			try {
-				System.out.println("http://" + server + ":9551/solr/MoreLikeThisIndex/select?q=parent%3Atrue+AND+" + key + "%3A\"" + URLEncoder.encode(value, "UTF-8") + "\"&rows=0&wt=json&indent=true");
+				System.out.println("http://" + server + ":9551/solr/MoreLikeThisIndex/select?q=parent%3Atrue+AND+" + key + "%3A" + URLEncoder.encode(value, "UTF-8") + "&rows=0&wt=json&indent=true");
 				throw new IllegalArgumentException("[ERROR]: the number found cannot be zero!");
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -106,7 +99,9 @@ public class SampleURLGenerator {
 
 	private static String queryForURL(String server, String key, String value, int rowNumber) {
 		try {
-			URL url = new URL("http://" + server + ":9551/solr/MoreLikeThisIndex/select?q=parent%3Atrue+AND+" + key + "%3A\"" + URLEncoder.encode(value, "UTF-8") + "\"&start=" + rowNumber + "&rows=1&fl=id&wt=json&indent=true");
+			URI uri = new URI("http://" + server + ":9551/solr/MoreLikeThisIndex/select?q=parent%3Atrue+AND+" + key + "%3A" + URLEncoder.encode(value, "UTF-8") + "&start=" + rowNumber + "&rows=1&fl=id&wt=json&indent=true");
+			URL url = uri.toURL();
+
 			StringBuilder responseSb = new StringBuilder();
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 			for (String line; (line = br.readLine()) != null; ) {
@@ -119,21 +114,11 @@ public class SampleURLGenerator {
 			if(element.isJsonObject()) {
 				return element.getAsJsonObject().getAsJsonObject("response").getAsJsonArray("docs").get(0).getAsJsonObject().get("id").getAsString();
 			}
-		} catch (IOException e) {
+		} catch (IOException|URISyntaxException e) {
 			e.printStackTrace();
 		}
 
 		throw new IllegalArgumentException("You should not be here!");
-	}
-
-	private static Set<Integer> generateRandomNumberSet(int size, int upperBound) {
-		Set<Integer> randomNumbers = new TreeSet<>();
-
-		while(randomNumbers.size() != size) {
-			randomNumbers.add(randomObject.nextInt(upperBound));
-		}
-
-		return randomNumbers;
 	}
 
 	// sort bins by fractional part, which is calculated as bin size / total size
@@ -141,11 +126,9 @@ public class SampleURLGenerator {
 	// randomly select X elements from each bin
 	// return that list and merge with the master list
 	// VERY IMPORTANT THAT SAMPLE SIZE IS DIVISIBLE BY 100 (makes things nice at the end)
-	private static List<String> stratifiedSample(List<BoundedBin> bins) {
-		List<String> sampleValues = new ArrayList<>();
-
+	private static void stratifiedSample(String schemaKey, List<Bin> bins) {
 		int populationSize = 0;
-		for(BoundedBin b : bins) {
+		for(Bin b : bins) {
 			for(int value : b.getMapping().values()) {
 				populationSize += value;
 			}
@@ -186,30 +169,47 @@ public class SampleURLGenerator {
 		// simple random sample from each bin
 		for(int i = 0; i < bins.size(); i++) {
 			if(percentageFromEachBin.get(i) != 0) {
-				sampleValues.addAll(simpleRandomSampleFromBin(bins.get(i), percentageFromEachBin.get(i) * SAMPLE_SIZE / 100));
+				addUniqueURLsFromBin(schemaKey, bins.get(i), percentageFromEachBin.get(i) * SAMPLE_SIZE / 100);
 			}
 		}
-
-		return sampleValues;
 	}
 
-	private static List<String> simpleRandomSampleFromBin(BoundedBin bin, int amount) {
-		List<String> sampleValues = new ArrayList<>();
-		List<Integer> randomNumbers = new ArrayList<>(generateRandomNumberSet(amount, bin.size()));
+	// simple random sampling
+	private static void addUniqueURLsFromBin(String schemaKey, Bin bin, int amount) {
+		int counter = 0;
+		String server = "grok.ics.uci.edu";
+		while(counter < amount) {
+			int rand = randomObject.nextInt(bin.size());
+			int index = 0;
 
-		int currentIndex = 0;
-		int randomNumberCounter = 0;
+			for(Map.Entry<String, Integer> entry : bin.getMapping().entrySet()) {
+				if(rand < index) {
+					String classURL;
+					if(schemaKey.equals("snippet_author_name") || schemaKey.equals("snippet_project_owner")) {
+						String newKey = "\"" + entry.getKey() + "\"";
+						int numFound = getNumFound(server, schemaKey, newKey);
+						classURL = queryForURL(server, schemaKey, newKey, randomObject.nextInt(numFound));
+					}
+					else {
+						int numFound = getNumFound(server, schemaKey, entry.getKey());
+						classURL = queryForURL(server, schemaKey, entry.getKey(), randomObject.nextInt(numFound));
+					}
 
-		for(Map.Entry<String, Integer> entry : bin.getMapping().entrySet()) {
-			currentIndex += entry.getValue();
+					if(!savedURLs.contains(classURL)) {
+						savedURLs.add(classURL);
+						counter++;
 
-			while(randomNumberCounter < randomNumbers.size() && currentIndex > randomNumbers.get(randomNumberCounter)) {
-				sampleValues.add(entry.getKey());
-				randomNumberCounter++;
+						UsefulThings.generateProgressBar(counter, amount);
+					}
+
+					break;
+				}
+
+				index += entry.getValue();
 			}
 		}
 
-		return sampleValues;
+		UsefulThings.generateProgressBar(counter, amount);
 	}
 
 	private static Map<String, Integer> populateWithCSVFile(String fileName) {
@@ -235,26 +235,8 @@ public class SampleURLGenerator {
 		return map;
 	}
 
-	private static List<String> createSampleValues(List<TrueFalseBin> bins) {
-		List<String> sampleValues = new ArrayList<>();
-		Set<Integer> randomNumbers = generateRandomNumberSet(SAMPLE_SIZE, POPULATION_SIZE);
-
-		for(int randomNumber : randomNumbers) {
-			int sumOfBinSizes = 0;
-			for(TrueFalseBin bin : bins) {
-				if(randomNumber < sumOfBinSizes + bin.size()) {
-					sampleValues.add(bin.getKey());
-					break;
-				}
-				sumOfBinSizes += bin.size();
-			}
-		}
-
-		return sampleValues;
-	}
-
-	private static List<BoundedBin> createEmptyBins(int n, int numberOfBins) {
-		List<BoundedBin> listOfBins = new ArrayList<>();
+	private static List<Bin> createEmptyBoundedBins(int n, int numberOfBins) {
+		List<Bin> listOfBins = new ArrayList<>();
 
 		int binWidth = (int)Math.ceil((double)n / numberOfBins);
 
@@ -270,15 +252,46 @@ public class SampleURLGenerator {
 	}
 
 	private static String escapeCharactersForSolr(String input) {
-		return input.replace("\\", "\\\\");
+		return input
+						.replace("\\", "\\\\")
+						.replace("\"", "\\\"")
+						.replace("[", "\\[")
+						.replace("]", "\\]")
+						.replace("(", "\\(")
+						.replace(")", "\\)")
+						.replace("{", "\\{")
+						.replace("}", "\\}")
+						.replace("*", "\\*")
+						.replace("+", "\\+")
+						.replace("-", "\\-")
+						.replace("&", "\\&")
+						.replace("!", "\\!")
+						.replace("|", "\\|")
+						.replace("~", "\\~")
+						.replace("?", "\\?")
+						.replace(";", "\\;")
+						.replace(":", "\\:")
+						.replace(" ", "\\ ")
+						.replace("^", "\\^");
 	}
 
 	// put data into bins
-	// this fails for true/false
-	private static void populateBins(Map<String, Integer> data, List<BoundedBin> bins) {
+	// number of bins equals number of keys in map
+	private static void populateBins(Map<String, Integer> data, List<Bin> bins) {
+		int counter = 0;
 		for(Map.Entry<String, Integer> entry : data.entrySet()) {
-			for(BoundedBin bin : bins) {
-				if(entry.getValue() >= bin.lowerBound && entry.getValue() < bin.upperBound) {
+			bins.get(counter).add(entry.getKey(), entry.getValue());
+			counter++;
+		}
+	}
+
+	// put data into bins
+	// number of bins calculated using FD
+	private static void populateBoundedBins(Map<String, Integer> data, List<Bin> bins) {
+		for(Map.Entry<String, Integer> entry : data.entrySet()) {
+			for(Bin bin : bins) {
+				BoundedBin b = (BoundedBin)bin;
+				if(entry.getValue() >= b.lowerBound && entry.getValue() < b.upperBound) {
 					bin.add(escapeCharactersForSolr(addPunctuationMarks(entry.getKey())), entry.getValue());
 					break;
 				}
@@ -286,31 +299,28 @@ public class SampleURLGenerator {
 		}
 	}
 
-	public static void main(String[] args) {
-		randomObject = new Random();
-
+	// start
+	private static void init() {
 		Map<String, Boolean> schemaKeys = new HashMap<>();
 		{
-			schemaKeys.put("snippet_imports", true);
-			schemaKeys.put("snippet_variable_names", true);
-			schemaKeys.put("snippet_class_name", true);
-			schemaKeys.put("snippet_author_name", true);
-			schemaKeys.put("snippet_project_name", true);
+//			schemaKeys.put("snippet_imports", true);
+//			schemaKeys.put("snippet_variable_names", true);
+//			schemaKeys.put("snippet_class_name", true);
+//			schemaKeys.put("snippet_author_name", true);
+//			schemaKeys.put("snippet_project_name", true);
 			schemaKeys.put("snippet_method_invocation_names", true);
-			schemaKeys.put("snippet_method_dec_names", true);
-			schemaKeys.put("snippet_size", true);
-			schemaKeys.put("snippet_imports_count", true);
-			schemaKeys.put("snippet_complexity_density", true);
-			schemaKeys.put("snippet_extends", true);
-			schemaKeys.put("snippet_package", true);
-			schemaKeys.put("snippet_number_of_fields", true);
-			schemaKeys.put("snippet_is_generic", false); // fails because of FD calculation
-			schemaKeys.put("snippet_is_abstract", false); // fails because of FD calculation
-			schemaKeys.put("snippet_is_wildcard", false); // fails because of FD calculation
-			schemaKeys.put("snippet_project_owner", true);
+//			schemaKeys.put("snippet_method_dec_names", true);
+//			schemaKeys.put("snippet_size", true);
+//			schemaKeys.put("snippet_imports_count", true);
+//			schemaKeys.put("snippet_complexity_density", true);
+//			schemaKeys.put("snippet_extends", true);
+//			schemaKeys.put("snippet_package", true);
+//			schemaKeys.put("snippet_number_of_fields", true);
+//			schemaKeys.put("snippet_is_generic", false); // fails because of FD calculation
+//			schemaKeys.put("snippet_is_abstract", false); // fails because of FD calculation
+//			schemaKeys.put("snippet_is_wildcard", false); // fails because of FD calculation
+//			schemaKeys.put("snippet_project_owner", true);
 		}
-
-		Map<String, List<String>> saved = new HashMap<>(); // this will be used to save the values from the stratified sampling + SRS
 
 		for(Map.Entry<String, Boolean> entry : schemaKeys.entrySet()) {
 			String schemaKey = entry.getKey();
@@ -318,49 +328,30 @@ public class SampleURLGenerator {
 
 			Map<String, Integer> fieldToOccurrenceMap = Statistics.sortByValue(populateWithCSVFile(schemaKey));
 
+			List<Bin> bins = new ArrayList<>();
+
 			if(entry.getValue()) {
 				List<Integer> valuesList = new ArrayList<>(fieldToOccurrenceMap.values());
-				List<BoundedBin> bins = createEmptyBins(valuesList.size(), Statistics.calculateFreedmanDiaconisChoice(Statistics.calculateIQR(valuesList), valuesList.size()));
-				populateBins(fieldToOccurrenceMap, bins);
-
-				stratifiedSample(bins).forEach(value -> {
-					saved.putIfAbsent(schemaKey, new ArrayList<>());
-					saved.get(schemaKey).add(value);
-				});
+				bins = createEmptyBoundedBins(valuesList.size(), Statistics.calculateFreedmanDiaconisChoice(Statistics.calculateIQR(valuesList), valuesList.size()));
+				populateBoundedBins(fieldToOccurrenceMap, bins);
 			}
 			else {
-				List<TrueFalseBin> bins = new ArrayList<>();
-				fieldToOccurrenceMap.forEach((field, occurrence) -> bins.add(new TrueFalseBin(field, occurrence)));
-				saved.put(schemaKey, createSampleValues(bins));
-			}
-		}
-
-		String server = "grok.ics.uci.edu";
-
-		for(Map.Entry<String, List<String>> entry : saved.entrySet()) {
-			Set<String> savedURLs = new HashSet<>(); // this will be used to get values using SRS
-
-			int counter = 0;
-
-			for(String value : entry.getValue()) {
-				int numFound = getNumFound(server, entry.getKey(), value);
-
-				UsefulThings.generateProgressBar(counter, SAMPLE_SIZE);
-
-				// query until you can add something to the set
-				while(true) {
-					String classURL = queryForURL(server, entry.getKey(), value, randomObject.nextInt(numFound));
-					if(!savedURLs.contains(classURL)) {
-						savedURLs.add(classURL);
-						counter++;
-						break;
-					}
+				for(int i = 0; i < fieldToOccurrenceMap.size(); i++) {
+					bins.add(new Bin());
 				}
+
+				populateBins(fieldToOccurrenceMap, bins);
 			}
 
-			UsefulThings.generateProgressBar(counter, SAMPLE_SIZE);
-
-			UsefulThings.printDataStructureToFile(savedURLs, "java_output/final_URL_list.txt");
+			stratifiedSample(schemaKey, bins);
 		}
+
+		UsefulThings.printDataStructureToFile(savedURLs, "java_output/final_urls.txt");
+	}
+
+	public static void main(String[] args) {
+		randomObject = new Random();
+		savedURLs = new HashSet<>();
+		init();
 	}
 }
